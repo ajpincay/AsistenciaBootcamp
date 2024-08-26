3d0494d18bb36c4b52dc32d5a63d58c1bc8c8e0d
Mateo <mchavez.castillo23@gmail.com>
2024-08-26 17:22:07 -0500

    Presente Mateo Chávez

--- src/Asistencia/src/main/java/org/example/Main.java
+++ src/Asistencia/src/main/java/org/example/Main.java
@@ -6,6 +6,17 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Presente: Angel Pincay");
+	
+
+
+
+
+
+
+
+
+
+
 	System.out.println("Presente: Mateo Chávez");
 
     }
