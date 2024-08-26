417a3e5c483eee570d46ff6fa506fd6f8e6ca4d8
Helen Bernal <133719584+bhelening@users.noreply.github.com>
2024-08-26 18:00:18 -0500

    Helen Bernal

--- src/Asistencia/src/main/java/org/example/Main.java
+++ src/Asistencia/src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Presente: Angel Pincay");
+        System.out.println("Presente: Helen Bernal");
         System.out.println("Presente: Leynder Sánchez");
         System.out.println("Presente: Daniel Olivo");
         
