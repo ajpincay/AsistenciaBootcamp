5250df69eae2f8e693e28ed5da475c7af82b918e
Jaesparz <jaesparz@espol.edu.ec>
2024-08-26 17:26:26 -0500

    Realicé cambios en el archivo main desde la rama josue-esparza

--- src/Asistencia/src/main/java/org/example/Main.java
+++ src/Asistencia/src/main/java/org/example/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Presente: Angel Pincay");
-        System.out.println("Presente: Leynder Sánchez");
+        System.out.println("Presente: Josue Esparza");
     }
 }
