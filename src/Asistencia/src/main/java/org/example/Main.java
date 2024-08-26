17ec85b6a45ce0d728179181ceb632d836ac2006
Ronaldo Rodriguez <rrodriguez@kvz.cl>
2024-08-26 17:20:33 -0500

    clerodri asistencia

--- src/Asistencia/src/main/java/org/example/Main.java
+++ src/Asistencia/src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Presente: Angel Pincay");
-        System.out.println("Presente: Ronaldo Rodriguez");
+        System.out.println("Presente: Ronaldo Rodriguez.");
 
     }
 }
