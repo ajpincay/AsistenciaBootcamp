2d7ec96e6426ecd4e579743c2dc83b8b31740702
AlexitoSnow <ganieves@espol.edu.ec>
2024-08-26 17:23:57 -0500

    Asistencia Geovanny Nieves

--- src/Asistencia/src/main/java/org/example/Main.java
+++ src/Asistencia/src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Presente: Angel Pincay");
+        System.out.println("Presente: Geovanny Nieves");
 
     }
 }
